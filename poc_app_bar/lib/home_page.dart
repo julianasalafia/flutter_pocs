3987bcc7a8f6213d2184abf41bb6370cfd88d530
Juliana Rousseau <juliana.salafia1@gmail.com>
2024-03-20 11:18:29 -0300

    feat: fixed sliver bar behavior

--- poc_app_bar/lib/home_page.dart
+++ poc_app_bar/lib/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:poc_app_bar/sliver_app_bar_delegate.dart';
 
 class TelaComSliverAppBar extends StatefulWidget {
   @override
@@ -6,47 +7,62 @@ class TelaComSliverAppBar extends StatefulWidget {
 }
 
 class _TelaComSliverAppBarState extends State<TelaComSliverAppBar> {
-  final ValueNotifier<bool> _showTitle = ValueNotifier<bool>(false);
+  final ScrollController _scrollController = ScrollController();
+  final ValueNotifier<bool> _isAppBarExpanded = ValueNotifier<bool>(false);
+
+  @override
+  void initState() {
+    super.initState();
+
+    _scrollController.addListener(() {
+      _isAppBarExpanded.value = _scrollController.hasClients && _scrollController.offset > (150 - kToolbarHeight);
+    });
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: CustomScrollView(
+        controller: _scrollController,
         slivers: <Widget>[
-          SliverAppBar(
+          const SliverAppBar(
+            surfaceTintColor: Colors.white,
+            elevation: 0,
             pinned: true,
             expandedHeight: 150.0,
             flexibleSpace: FlexibleSpaceBar(
-              title: LayoutBuilder(builder: (context, constraints) {
-                if (mounted)  _showTitle.value = constraints.maxHeight < 150.0;
-                return const Text('Título Fixo', style: TextStyle(color: Colors.red));
-              }),
+              title: Text('Flutter Roadmap', style: TextStyle(color: Colors.black54)),
             ),
           ),
-
+          SliverPersistentHeader(
+            pinned: true,
+            delegate: SliverAppBarDelegate(
+              minHeight: 1,
+              maxHeight: 1,
+              child: ValueListenableBuilder(
+                valueListenable: _isAppBarExpanded,
+                builder: (_, __, ___) {
+                  return Container(
+                    color: _isAppBarExpanded.value ? Colors.black54 : Colors.transparent,
+                  );
+                }
+              ),
+            ),
+          ),
           SliverToBoxAdapter(
             child: Column(
               children: [
-                ValueListenableBuilder<bool>(
-                  valueListenable: _showTitle,
-                  builder: (context, showTitle, child) {
-
-                    return showTitle
-                        ? const SizedBox.shrink()
-                        : const SizedBox(
-                      height: 30.0,
-                      child: Center(
-                        child: Divider(),
-                      ),
-                    );
-                  },
-                ),
-
+                const SizedBox(height: 20.0),
                 Text(
                   'Aqui vai o seu longo conteúdo de texto que deve ser rolável junto com o resto da página, mas mantendo o app bar fixo com o título no topo. ' * 50,
                   style: const TextStyle(fontSize: 16.0),
                 ),
-
               ],
             ),
           ),
