0d6c65cb09f16aa320b24bc8a0594376c782eae5
Juliana Rousseau <juliana.salafia1@gmail.com>
2025-09-25 20:00:54 -0300

    feat: create example with flutter bloc package

---
+++ poc_package_flutter_bloc/lib/blocs/client_state.dart
@@ -0,0 +1,15 @@
+import 'package:poc_package_flutter_bloc/models/client.dart';
+
+abstract class ClientState {
+  List<Client> clients;
+
+  ClientState({required this.clients});
+}
+
+class ClientInitialState extends ClientState {
+  ClientInitialState() : super(clients: []);
+}
+
+class ClientSuccessState extends ClientState {
+  ClientSuccessState({required super.clients});
+}
